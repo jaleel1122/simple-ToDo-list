4b1a072c968f608b7d4c6d25e8fa190046c9d5b5
jaleel1122 <shaikabduljaleel1214@gmail.com>
2025-08-23 16:29:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ ReactDOM.render(<App />, document.getElementById("root"));
 //2. When the add button is pressed, the current data in the input should be
 //added to an array.
 //3. The <ul> should display all the array items as <li>s
-console.log('heeee')
+
